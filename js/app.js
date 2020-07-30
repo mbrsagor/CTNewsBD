df6729c0ab8a9060fb3caf29c6bda8fc045f78c6
Sagor <mbrsagor@gmail.com>
2020-07-31 01:32:37 +0600

    Input type change to textarea

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
 function addTodo() {
     var nameValue = document.querySelector(".input-name").value;
-    var jobValue = document.querySelector(".input-job").value;
+    var description = document.querySelector(".input-description").value;
     var resultsBlock = document.querySelector(".results");
     var resultName = document.querySelector(".name-result");
-    var resultJob = document.querySelector(".job-result");
+    var resultDescription = document.querySelector(".title-result");
 
     resultName.innerHTML += nameValue;
-    resultJob.innerHTML += jobValue;
+    resultDescription.innerHTML += description;
     resultsBlock.classList.remove("hidden");
 }
 
